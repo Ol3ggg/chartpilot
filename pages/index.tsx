ccea4f2addb22641c2ececb7daae8e78c3ec2ab6
Ol3ggg <olegg_g@gmx.net>
2025-04-23 19:25:50 +0200

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from '../components/ui/card'
 
 export default function Home() {
   return (
